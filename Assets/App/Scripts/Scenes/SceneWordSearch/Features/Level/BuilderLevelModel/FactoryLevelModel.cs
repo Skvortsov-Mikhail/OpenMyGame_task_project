7de9a10a8411802d25eff0c17a05ef037ac9e3a7
Skvortsov-Mikhail <skw-misha@yandex.ru>
2023-09-13 20:54:53 +0300

    Выполнена вторая часть задания (WordSearch)
    Реализованы алгоритм получения букв, загрузка и парсинг уровней

--- Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/FactoryLevelModel.cs
+++ Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/FactoryLevelModel.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using UnityEngine;
 using App.Scripts.Libs.Factory;
 using App.Scripts.Scenes.SceneWordSearch.Features.Level.Models.Level;
 
@@ -21,8 +22,84 @@ namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel
 
         private List<char> BuildListChars(List<string> words)
         {
-            //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+            Dictionary<char, int> letterPairsInLevel = new Dictionary<char, int>();
+
+            foreach (string word in words)
+            {
+                Dictionary<char, int> letterPairsInWord = ParseWordToLetters(word);
+
+                foreach (var letter in letterPairsInWord.Keys)
+                {
+                    if (letterPairsInLevel.ContainsKey(letter))
+                    {
+                        int letterCountInLevelDictionary = letterPairsInLevel.GetValueOrDefault(letter);
+                        int letterCountInWordDictionary = letterPairsInWord.GetValueOrDefault(letter);
+
+                        letterPairsInLevel[letter] = Mathf.Max(letterCountInLevelDictionary, letterCountInWordDictionary);
+                    }
+                    else
+                    {
+                        letterPairsInLevel[letter] = letterPairsInWord.GetValueOrDefault(letter);
+                    }
+                }
+            }
+
+            List<char> builtList = FillLettersList(letterPairsInLevel);
+
+            Shuffle(builtList);
+
+            return builtList;
+        }
+
+        private Dictionary<char, int> ParseWordToLetters(string word)
+        {
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            foreach (char letter in word.ToLower().ToCharArray())
+            {
+                if (!Char.IsLetter(letter)) continue;
+
+                if (dictionary.ContainsKey(letter))
+                {
+                    dictionary[letter] = dictionary.GetValueOrDefault(letter) + 1;
+                }
+                else
+                {
+                    dictionary.Add(letter, 1);
+                }
+            }
+
+            return dictionary;
+        }
+
+        private List<char> FillLettersList(Dictionary<char, int> letters)
+        {
+            List<char> list = new List<char>();
+
+            foreach (char letter in letters.Keys)
+            {
+                for (int i = 0; i < letters.GetValueOrDefault(letter); i++)
+                {
+                    list.Add(letter);
+                }
+            }
+
+            return list;
+        }
+
+        private void Shuffle(List<char> array)
+        {
+            var random = new System.Random();
+            int n = array.Count;
+
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                char value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
         }
     }
 }
--- Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
+++ Assets/App/Scripts/Scenes/SceneWordSearch/Features/Level/BuilderLevelModel/ProviderWordLevel/ProviderWordLevel.cs
@@ -1,14 +1,26 @@
-using System;
+using System.Collections.Generic;
+using System.IO;
 using App.Scripts.Scenes.SceneWordSearch.Features.Level.Models.Level;
+using Newtonsoft.Json;
 
 namespace App.Scripts.Scenes.SceneWordSearch.Features.Level.BuilderLevelModel.ProviderWordLevel
 {
     public class ProviderWordLevel : IProviderWordLevel
     {
+        class JSONData
+        {
+            public List<string> words;
+        }
+
         public LevelInfo LoadLevelData(int levelIndex)
         {
-            //напиши реализацию не меняя сигнатуру функции
-            throw new NotImplementedException();
+            LevelInfo info = new LevelInfo();
+
+            string fileData = File.ReadAllText($"Assets/App/Resources/WordSearch/Levels/{levelIndex}.json");
+
+            info.words = JsonConvert.DeserializeObject<JSONData>(fileData).words;
+
+            return info;
         }
     }
 }
